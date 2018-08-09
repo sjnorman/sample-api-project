10e156bac18c94ec33ea087dc37f2086fbac2af5
Norman, Sean <Sean.Norman@londonlife.com>
2018-08-09 09:56:36 -0400

    Adding boilerplate classes

---
+++ src/main/java/com/sample/StringManipulator.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+public class StringManipulator {
+
+    public void someMethod() {
+
+    }
+
+}
---
+++ src/test/java/com/sample/StringManipulatorTest.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+import org.junit.Test;
+
+public class StringManipulatorTest {
+
+    @Test
+    public void someTest() {
+
+    }
+
+}
